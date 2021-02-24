5a34dccffe958fb0fc057df8cf6d2ff249d175d3
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-02-23 23:38:35 -0700

    set localstorage

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 var currentDate = document.getElementById("currentDay");
-var apptDescription = document.getElementById("#textarea")
+var apptDescription = document.getElementById("textarea")
 
 var todayDate = function() {
-var currentDatedisplay = moment().format("dddd, MMM, Do, YYYY");
+var currentDatedisplay = moment().format("dddd, MMM, Do, YYYY, LT");
 currentDate.textContent = currentDatedisplay
 };
 todayDate();
-
 
 time = moment().format("LT");
 
@@ -20,7 +19,89 @@ time = moment().format("LT");
 //   $(taskEl).addClass("list-group-item-warning");
 
 // }
+var time 
+var appt8 = document.getElementById("#time-block-8");
+
+document.getElementById("btn8").addEventListener("click", function(){
+ var appt8El = document.querySelector("#block8").value;
+   localStorage.setItem("btn8", appt8El);
+  
+})
+
+// var loadAppts = function() { 
+//   localStorage.getItem("btn8");
+//   appt8El.textContent = "btn8";
+// };
+
+// loadAppts()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("btn9").addEventListener("click", function(){
+  var appt9El = document.querySelector("#block9").value;
+  localStorage.setItem("btn9", appt9El);
+})
+
+document.getElementById("btn10").addEventListener("click", function(){
+  console.log("good job10")
+})
+
+document.getElementById("btn11").addEventListener("click", function(){
+  console.log("good job11")
+})
+
+document.getElementById("btn12").addEventListener("click", function(){
+  console.log("good job12")
+})
+
+document.getElementById("btn1").addEventListener("click", function(){
+  console.log("good job1")
+})
+document.getElementById("btn2").addEventListener("click", function(){
+  console.log("good job2")
+})
+
+document.getElementById("btn3").addEventListener("click", function(){
+  console.log("good job3")
+})
+
+document.getElementById("btn4").addEventListener("click", function(){
+  console.log("good job4")
+})
+
+document.getElementById("btn5").addEventListener("click", function(){
+  console.log("good job5")
+})
+
+
+
+//click on 
 // save appts to localstorage
-var saveApptss = function() {
-  localStorage.setItem("textarea", JSON.stringify(apptDescription));
-};
+
+
+
+
+
+// var saveAppts = function() {
+//   localStorage.setItem("textarea", JSON.stringify(apptDescription));
+// };
