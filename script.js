86cf73bf6582e10d8480ff141f1bf4f4941701d2
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-02-25 23:36:13 -0700

    trying to get the color/time change

--- script.js
+++ script.js
@@ -15,35 +15,40 @@ time = moment().format("LT");
 function dayDisplay() {
   var displayDay = $("<h2>")
   displayDay.addClass("lead")
-  displayDay.text( moment().format("dddd, MMM Do YYYY, LT"))
+  displayDay.text(moment().format("dddd, MMM Do YYYY, LT"))
   $("#currentDay").append(displayDay)
 }
 
 
 
 $(document).ready(function () {
- dayDisplay()
+  dayDisplay()
 
 });
 
 
 
 
-// var currentTime = function(blockNum) {
-// // apply new class if task is near/over due date
-// if (moment().isAfter(time); || moment().isAfter(time) ) {
-//   $("block"+ blockNum).removeClass("#past");
- 
-// } 
-// else if (moment().isBefore(time)) {
-//   $(taskEl).addClass("#future");
+function currentTime(blockNum) {
+  var today = new Date();
+  var hours = today.getUTCHours();
+  if (hours = blockNum) {
+    $("#block" + blockNum).addClass("present")
+  }
+  else if (hours < blockNum) {
+    $("#block" + blockNum).addClass("future")
+  }
+  else {
+    $("#block" + blockNum).addClass("past")
+  }
+}
 
-// }
+
 var time
 
 var handleAppt = function (blockNum) {
   var appt = $("#block" + blockNum)
-  $("#btn"+ blockNum).click(function(){
+  $("#btn" + blockNum).click(function () {
     var apptEl = $("#block" + blockNum).val()
     localStorage.setItem("btn" + blockNum, apptEl);
   })
@@ -56,6 +61,18 @@ var handleAppt = function (blockNum) {
   loadAppts();
 }
 
+currentTime(8);
+currentTime(9);
+currentTime(10);
+currentTime(11);
+currentTime(12);
+currentTime(13);
+currentTime(2);
+currentTime(3);
+currentTime(4);
+currentTime(5);
+
+
 handleAppt(8);
 handleAppt(9);
 handleAppt(10);
@@ -66,3 +83,4 @@ handleAppt(2);
 handleAppt(3);
 handleAppt(4);
 handleAppt(5);
+
